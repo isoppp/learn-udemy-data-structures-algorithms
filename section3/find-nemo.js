20cefbf2643d223baaa71addc465013448944799
isoppp <prog@d2deck.com>
2019-09-06 11:49:34 -0700

    Add findNemo function

---
+++ section3/find-nemo.js
@@ -0,0 +1,30 @@
+const { performance } = require('perf_hooks');
+const nemo = ['nemo'];
+const everyone = [
+  'dory',
+  'bruce',
+  'marlin',
+  'nemo',
+  'gill',
+  'bloat',
+  'nigel',
+  'squirt',
+  'derla',
+  'hank',
+];
+const large = new Array(100).fill('nemo');
+
+function findNemo(array) {
+  let t0 = performance.now();
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') {
+      console.log('Found NEMO!');
+    }
+  }
+
+  let t1 = performance.now();
+  console.log('Call to find Nemo took' + (t1 - t0) + 'milliseconds');
+}
+
+findNemo(large);
