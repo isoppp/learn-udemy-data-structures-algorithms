98d4de298fef73ec2af2c9a6a3d86acdbf7ffc27
isoppp <prog@d2deck.com>
2019-09-11 09:39:18 -0700

    Add my append function for linked-list

--- data-structures/linked-list/linked-list-implementation.js
+++ data-structures/linked-list/linked-list-implementation.js
@@ -17,15 +17,42 @@ class LinkedList {
       value,
       next: null,
     };
+
     this.tail = this.head;
     this.length = 1; // optional
   }
+
   append(value) {
-    // TODO next
+    const newNode = {
+      value,
+      next: null,
+    };
+
+    if (this.head.next === null) {
+      this.head.next = newNode;
+      this.tail = newNode;
+    } else {
+      let node = this.head;
+      while (true) {
+        if (node.next === null) {
+          node.next = newNode;
+          this.tail = newNode;
+          break;
+        }
+        node = node.next;
+      }
+    }
   }
 }
 
 const myLinkedList = new LinkedList(10);
+// console.log(myLinkedList);
+
 myLinkedList.append(5);
+// console.log(myLinkedList);
+
 myLinkedList.append(16);
+myLinkedList.append(12);
+myLinkedList.append(11);
 console.log(myLinkedList);
+console.log(JSON.stringify(myLinkedList.head, null, 2));
